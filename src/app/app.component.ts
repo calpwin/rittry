bdc1eeae1c866b3095ac69f46dfa35ecd59b597f
arotaev <arotaev@ozon.ru>
2021-09-03 23:29:31 +0300

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rittry-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'rittry';
+}
